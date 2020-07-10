271f41928b932314a045e6e38df6fded309b9638
Rachael Ghorbani <rachael.ghorbani@gmail.com>
2020-07-10 15:25:35 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -4,7 +4,8 @@ def mario
 end
 
 def toadstool
+  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts status
 end  
 
-#status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+def 
